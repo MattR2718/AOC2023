71debafde23899bf6ea8aca5d8f42f1213cbe8fc
MattR2718 <matthew.ranson2718@gmail.com>
2023-12-09 17:45:33 +0000

    day9 ctre comparison

--- day9/day9.cpp
+++ day9/day9.cpp
@@ -11,6 +11,8 @@
 #include <functional>
 #include <chrono>
 
+#include <ctre.hpp>
+
 auto getInput(const std::string f = "input.txt") {
     std::ifstream file(f);
     std::string linetxt;
@@ -27,6 +29,22 @@ auto getInput(const std::string f = "input.txt") {
         for (std::sregex_iterator i = begin; i != end; i++) {
             m = *i;
             temp.push_back(std::stoll(m.str()));
+        }
+        out.push_back(temp);
+    }
+
+    return out;
+}
+
+auto getInputCTRE(const std::string f = "input.txt") {
+    std::ifstream file(f);
+    std::string linetxt;
+    std::vector<std::vector<int>> out;
+
+    while (std::getline(file, linetxt)) {
+        std::vector<int> temp;
+        for (const auto& m : ctre::search_all<"(-?\\d+)">(linetxt)) {
+            temp.push_back(m.to_number<int>());
         }
         out.push_back(temp);
     }
@@ -110,12 +128,48 @@ auto runWithInputFetching(std::string f = "input.txt") {
     std::cout << "PART 1: " << p1 << "\nPART 2: " << p2 << '\n';
 }
 
+auto runWithInputFetchingCTRE(std::string f = "input.txt") {
+    std::ifstream file(f);
+    std::string linetxt;
+    std::vector<std::vector<int>> out;
+
+    std::regex ex("(-?\\d+)");
+    std::smatch m;
+
+    int p1 = 0;
+    int p2 = 0;
+
+    while (std::getline(file, linetxt)) {
+        std::vector<int> input;
+        for (const auto& m : ctre::search_all<"(-?\\d+)">(linetxt)) {
+            input.push_back(m.to_number<int>());
+        }
+
+        auto num = calcDiff(input);
+        p1 += input[input.size() - 1] + num;
+
+        std::transform(std::execution::par, input.cbegin(), input.cend(), input.begin(), std::negate<int>());
+        std::reverse(input.begin(), input.end());
+
+        num = calcDiff(input);
+        p2 += -1 * (input[input.size() - 1] + num);
+
+    }
+
+    std::cout << "PART 1: " << p1 << "\nPART 2: " << p2 << '\n';
+}
+
 
 int main() {
     auto start = std::chrono::high_resolution_clock::now();
     auto input = getInput();
     auto end = std::chrono::high_resolution_clock::now();
     auto inputT = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+
+    start = std::chrono::high_resolution_clock::now();
+    input = getInputCTRE();
+    end = std::chrono::high_resolution_clock::now();
+    auto inputCTRET = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
 
     /*for (auto& h : input) {
         for (auto& i : h) {
@@ -133,7 +187,7 @@ int main() {
     end = std::chrono::high_resolution_clock::now();
     auto part2T = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
 
-    std::cout << "INPUT GOT IN: " << inputT << "\nPART 1 DONE IN: " << part1T << "\nPART 2 DONE IN: " << part2T << "\n";
+    std::cout << "INPUT GOT IN: " << inputT << "\nCTRE INPUT GOT IN: " << inputCTRET << "\nPART 1 DONE IN: " << part1T << "\nPART 2 DONE IN: " << part2T << "\n";
 
 
     std::cout << "DOING ALL AT ONCE\n";
@@ -142,6 +196,13 @@ int main() {
     end = std::chrono::high_resolution_clock::now();
     auto all = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
     std::cout << "RAN IN: " << all << '\n';
+
+    std::cout << "DOING ALL AT ONCE CTRE\n";
+    start = std::chrono::high_resolution_clock::now();
+    runWithInputFetchingCTRE();
+    end = std::chrono::high_resolution_clock::now();
+    auto allCTRE = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+    std::cout << "RAN IN: " << allCTRE << '\n';
 }
 
 //Part 1: 1798691765
@@ -163,23 +224,28 @@ int main() {
 
 //Part 1: 1798691765
 //Part 2: 1104
-//INPUT GOT IN : 7283us
-//PART 1 DONE IN : 534us
-//PART 2 DONE IN : 742us
+//INPUT GOT IN : 8197us
+//CTRE INPUT GOT IN : 734us
+//PART 1 DONE IN : 579us
+//PART 2 DONE IN : 792us
 //DOING ALL AT ONCE
 //PART 1 : 1798691765
 //PART 2 : 1104
-//RAN IN : 7925us
+//RAN IN : 8448us
+//DOING ALL AT ONCE CTRE
+//PART 1 : 1798691765
+//PART 2 : 1104
+//RAN IN : 2480us
 //
 //
 //Days : 0
 //Hours : 0
 //Minutes : 0
 //Seconds : 0
-//Milliseconds : 29
-//Ticks : 298337
-//TotalDays : 3.45297453703704E-07
-//TotalHours : 8.28713888888889E-06
-//TotalMinutes : 0.000497228333333333
-//TotalSeconds : 0.0298337
-//TotalMilliseconds : 29.8337
+//Milliseconds : 34
+//Ticks : 348199
+//TotalDays : 4.03008101851852E-07
+//TotalHours : 9.67219444444444E-06
+//TotalMinutes : 0.000580331666666667
+//TotalSeconds : 0.0348199
+//TotalMilliseconds : 34.8199
